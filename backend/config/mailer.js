2a02780092f586724ce0ce92b252b581564de475
Pawan Singh <pawanpundir191@gmail.com>
2026-03-06 02:09:46 +0530

    fix: mailer timeout

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -8,7 +8,9 @@ console.log(
 );
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
